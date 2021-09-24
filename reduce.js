cee1489640bf3a5b5a90278f50022ce74d36c615
BaileyMuir <BaileyIRMuir@outlook.com>
2021-09-24 23:38:41 +0000

    Add examples.

--- reduce.js
+++ reduce.js
@@ -6,7 +6,32 @@
 
 
 // Summing an array of numbers:
+const num = [0, 1, 2, 3, 4];
+let sum = num.reduce((acc, curr) => {
+  console.log(
+    "accumulator:", acc,
+    "current value:", curr,
+    "total:", acc + curr
+  );
+  return acc + curr;
+});
+console.log(sum)
+// or bellow is better as its provided a base value
 
+const nums = [0, 1, 2, 3, 4];
+let sums = nums.reduce((acc, curr) => {
+  console.log(
+    "accumulator:", acc,
+    "current value:", curr,
+    "total:", acc + curr
+  );
+  return acc + curr;
+}, 10);
+console.log(sums)
+// short hand
+
+const sums2 = num.reduce((acc, curr) => acc+ curr, 0);
+console.log(sums2)
 
 const teamMembers = [
   {
@@ -33,5 +58,18 @@ const teamMembers = [
 
 // Totaling a specific object property
 
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience)
 
 // Grouping by a property, and totaling it too
+let exsperienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(exsperienceByProfession)
